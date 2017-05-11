739dac2b7303a63b1008b19a77d60e80090afddb
Wes Payne <wpayne@marchex.com>
2017-05-10 23:10:45 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# removechef
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: removechef
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
